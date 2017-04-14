f8541168c593023db466c5b04cc56739091eaffd
aseem arora <aseemarora1995@gmail.com>
2017-04-15 01:33:16 +0530

    stack class

---
+++ CD/06.04.17; PredictiveParser/stack.py
@@ -0,0 +1,15 @@
+class MyStack:
+	def __init__(self):
+		self.items = []
+
+	def push(self, item):
+		self.push(item)
+	
+	def pop():
+		self.pop()
+	
+	def isEmpty():
+		return self.items == []
+
+	def size():
+		return len(self.items)
