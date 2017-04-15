3fd607e3b3d8cb2ded54d0047007010f2f7f030f
aseem arora <aseemarora1995@gmail.com>
2017-04-15 15:15:56 +0530

    correct stack class

--- CD/06.04.17; PredictiveParser/stack.py
+++ CD/06.04.17; PredictiveParser/stack.py
@@ -1,15 +1,21 @@
-class MyStack:
+class MyStack(object):
 	def __init__(self):
 		self.items = []
 
 	def push(self, item):
-		self.push(item)
+		self.items.append(item)
 	
-	def pop():
-		self.pop()
+	def pop(self):
+		self.items.pop()
 	
-	def isEmpty():
+	def isEmpty(self):
 		return self.items == []
 
-	def size():
+	def size(self):
 		return len(self.items)
+
+	def top(self):
+		return self.items[-1]
+
+	def show(self):
+		print self.items
